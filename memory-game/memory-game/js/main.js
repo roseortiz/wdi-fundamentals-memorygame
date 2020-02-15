da078b25e51170c4c2cde097441c0321426a634c
Rose <roseortiz0603@yahoo.com>
2020-02-15 13:47:29 -0800

    created game logic

--- memory-game/memory-game/js/main.js
+++ memory-game/memory-game/js/main.js
@@ -1,6 +1,16 @@
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+const cards = ["queen", "queen", "king", "king"];
+cardsInPlay = [];
+let cardOne = (cards[0]);
+cardsInPlay.push('cardOne');
+let cardTwo = (cards[2]);
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
+if (cardsInPlay.length = 2){ 
+
+	console.log("You have selected two cards.");
+} else if (cardsInPlay[0] === (cardsInPlay[2]) {
+	alert("You found a match!");
+} else { 
+	alert("Sorry, try again.");
+};
+
